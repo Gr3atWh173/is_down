4ee9b555b8fe91df1e016db72219248e42008bf6
Gr3atWh173 <Gr3atWh173@users.noreply.github.com>
2018-01-10 20:48:24 +0530

    add documentation

--- lib/is_down.rb
+++ lib/is_down.rb
@@ -1,13 +1,19 @@
-#require "is_down/version"
+require "is_down/version"
 require "net/http"
 
+# check if a website is up or not using downforeveryoneorjustme.com
 module IsDown
   ISUPME = "http://downforeveryoneorjustme.com"
 
+  # check if a website is down
+  # takes a domain name as the only argument
   def self.is_down?(host)
     !is_up?(host)  
   end
 
+  # opposite of is_down?
+  # checks if a website is up
+  # takes a domain name as the only argument
   def self.is_up?(host)
     Net::HTTP.get_response(URI(ISUPME + "/" + host)).body.include?("is up")
   end
